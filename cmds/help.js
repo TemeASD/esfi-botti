3329d93a2fe73cbf79c9bd3ed23f3da8275f4021
TemeASD <temesus@hotmail.com>
2020-09-19 14:05:20 +0300

    started working on captains

--- cmds/help.js
+++ cmds/help.js
@@ -7,11 +7,20 @@ module.exports = {
     execute: async (bot, message, args) => {
         const embed = new Discord.MessageEmbed()
             .setColor('#42d1f5')
-            .setTitle('esfibot commands')
-            .addField('!join', 'Lisää itsesi pelaajarosteriin')
-            .addField('!out', 'Poista itsesi pelaajarosterista')
-            .addField('!clear', 'Tyhjennä rosteri')
-            .addField('!rosteri', 'Näytä pelaajarosteri' )
+            .setTitle('esfibot komennot')
+            .addFields(
+                {name: '!join', value: 'Lisää itsesi pelaajarosteriin', inline: true },
+                {name: '!out', value:'Poista itsesi pelaajarosterista', inline: true },
+                {name: '!clear', value:'Tyhjennä rosteri', inline: true },
+                {name: '!rosteri', value:'Näytä pelaajarosteri', inline: true } 
+                )
+            .addField('SCRIMS', ' ')
+            .addFields( 
+                {name: '!scrim start map', value: 'Käynnistää palvelimen', inline: true},
+                {name: '!scrim end',  value:'Lopettaa pelin ja sammuttaa palvelimen', inline: true}, 
+                {name: '!scrim clear',  value:'Tyhjentää joukkueet', inline: true},
+                {name: '!scrim team1/team2',  value: 'Asettaa viestin lähettäneen pelaajan haluttuun joukkueeseen', inline: true},
+                {name: '!scrim team1/team2 @pelaaja',  value: 'Asettaa tägätyn pelaajan haluttuun joukkueeseen' , inline: true})
             .setFooter('(c) TemeASD#1134','','https://github.com/TemeASD/esfi-botti')
         message.channel.send(embed);
     }
--- cmds/scrim.js
+++ cmds/scrim.js
@@ -4,7 +4,7 @@ const { CSGO_PATH, CSGO_srv_token } = require('../config.json');
 const { exec } = require('child_process');
 let connect = ''
 function connectString() {
-     exec('curl bot.whatismyipaddress.com', (err, stdout, stderr) => {
+    exec('curl bot.whatismyipaddress.com', (err, stdout, stderr) => {
         if (err) {
             connect = `lol joku meni vikaan :D`;
         } else {
@@ -63,13 +63,12 @@ scrim.json and these arrays *WILL* desync in case of bot crash
 
 TODO: fix :D
 */
-let team1=[]
-let team2=[]
+let team1 = []
+let team2 = []
 module.exports = {
     name: 'scrim',
     description: 'Assign yourself to a team then start a server to jump right in',
     execute: async (bot, message, args, child) => {
-        console.log(child)
         if (!args.length) {
             fs.readFile('./games/scrim.json', (err, content) => {
                 if (err) return console.error(err);
@@ -84,55 +83,44 @@ module.exports = {
         fs.readFile('./games/steamid.json', (err, content) => {
             if (err) return console.error(err);
             const steamIDs = JSON.parse(content);
-
             if (Object.keys(steamIDs).indexOf(message.author.id) === -1) return message.channel.send(`Steam ID:tä ei löytynyt "tietokannasta(:D)" käytä \`!steamid\` komentoa.`);
-        }); 
+        });
 
+        /*Assigning captain to a team 1 */
+        if (args[0] === 'captain' && args[1] === 'team1') {
+            if (team1.length === 0) {
+                addToTeam(args[1], message.author.id, message.author.username, message)
+            } else {
+                return message.channel.send('Tiimissä on jo kapteeni')
+            }
+        }
+        /*Assigning captain to a team 2 */
+        if (args[0] === 'captain' && args[1] === 'team2') {
+            if (team2.length === 0) {
+                addToTeam(args[1], message.author.id, message.author.username, message)
+            } else {
+                return message.channel.send('Tiimissä on jo kapteeni')
+            }
+        }
         if (args[0] === 'team1' || args[0] === 'team2') {
-            fs.readFile('./games/scrim.json', async (err, content) => {
-                if (err) return console.error(err);
-                let roster = JSON.parse(content);
-
-                delete roster['team1'][message.author.id];
-                delete roster['team2'][message.author.id];
-
-                fs.readFile('./games/steamid.json', (err, content) => {
-                    if (err) return console.error(err);
-                    const steamid = JSON.parse(content);
-                    const discordid = message.author.id;
-
-                    roster[args[0]][discordid] = steamid[discordid];
-                    fs.writeFile('./games/scrim.json', JSON.stringify(roster, null, '\t'), err => {
-                        if (err) return console.error(err);
-
-                        team1 = team1.filter(name => {return name != message.author.username})
-                        team2 = team2.filter(name => {return name != message.author.username})
-                        if (args[0] === 'team1') { team1.push(message.author.username)}
-                        if (args[0] === 'team2') { team2.push(message.author.username)}
-                        
-                        const embed = new Discord.MessageEmbed().setTitle('5v5 Scrim Roster').setColor('#ff5555')
-                            .addField(`Tiimi 1 - ${team1.length}`, team1.join('\n') || 'empty', true)
-                            .addField(`Tiimi 2 - ${team2.length}`, team2.join('\n') || 'empty', true);
-                        message.channel.send(embed);                    });
-                });
-            });
+            /*had second argument, but not mentioned any other user, not doing shit*/
+            if (args[1] && message.mentions.users.size === 0) {
+                return message.channel.send('Tägää joku pelaaja. Jos haluat lisätä itsesi tiimiin, älä laita muuta kuin `!scrim teamN`')
+            }
+            
+            /*had second argument, other user mentioned, adding to roster*/
+            if (!args[1] && message.mentions.users.size === 1) {
+                let mentionedUser = Array.from(message.mentions.users.values())[0];
+                addToTeam(args[0], mentionedUser.id, mentionedUser.username, message);
+            }
+            /*no arguments, adding self to the wanted team*/
+            if (!args[1]) {
+                addToTeam(args[0], message.author.id, message.author.username, message)
+            }
         }
 
-        if (args[0] === 'clear') {
-            const roster = {
-                "team1": {},
-                "team2": {}
-            };
-            team1=[]
-            team2=[]
-            fs.writeFile('./games/scrim.json', JSON.stringify(roster, null, '\t'), err => {
-                if (err) return console.error(err);
-                message.channel.send('Scrimin rosteri tyhjennetty.');
-            });
-        }
 
         if (args[0] === 'end') {
-            console.log(child, 'asd')
             child.stdin.write(`quit\n`);
             message.channel.send('Serveri sammuu....');
             const roster = {
@@ -144,68 +132,105 @@ module.exports = {
                 message.channel.send('Scrimin rosteri tyhjennetty.');
             });
         }
-
+        /* move to separate function/command*/
         if (args[0] === 'start') {
+            /*validation from arguments*/
             if (!args[1]) return message.channel.send('Anna mappi, esim "de_dust2"');
-
-            // check for correct map name
-            const maps = ["de_nuke", "de_dust2", "de_vertigo", "de_inferno", "de_overpass", "de_train", "de_mirage"]
+            const maps = ["de_nuke", "de_dust2", "de_vertigo", "de_inferno", "de_overpass", "de_train", "de_mirage", "de_anubis", "de_cbble"]
             if (!maps.includes(args[1])) return message.channel.send(`Validit kartat: ${maps.toString()} `);
 
+            /*if succeeded lets read the ready scrim json and do some validation*/
             fs.readFile('./games/scrim.json', (err, content) => {
                 if (err) return console.error(err);
                 const roster = JSON.parse(content);
                 if (Object.values(roster.team1).length != 5 && Object.values(roster.team2).length != 5) return message.channel.send('Joukkeissa ei tarpeeksi pelaajia.');
                 if (!Object.keys(roster.team1).includes(message.author.id) && !Object.keys(roster.team2).includes(message.author.id)) return message.channel.send('Et ole rosterissa, haista paska :D');
 
+                /*validation succeeded, lets load the the team 1 and write scrim template*/
+                /*get5 scrims only want "team1", no need to worry about team2 anyone else*/
                 const rosterArray = Object.values(roster.team1);
                 const scrimRoster = new Roster(rosterArray);
-
-                fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, scrimRoster.config, async err => {
-                    if (err) return console.error(err);
-                    console.log('tunkataan servu ylös')
-                    child.stdin.write(`./srcds_run -game csgo -tickrate 128 -net_port_try 1 -console -condebug -usercon +game_type 0 +game_mode 1 +map ${args[1]} +maxplayers 12 +sv_setsteamaccount ${CSGO_srv_token}\n`);
-                    const msg = await message.channel.send('Sierra hyrskyttää...');
-                    connectString()
-                    bot.setTimeout(() => {
-                        msg.edit(`\`${connect}\``);
-                    }, 10000);
-                });
+                startServer(scrimRoster, args[1], message, child, bot);
             });
         }
 
+        if (args[0] === 'clear') {
+            clearRoster(message);
+            clearTeams();
+        }
         if (args[0] === 'test') {
             if (message.author.id != '183934154558275584') return;
             const rosterArray = ['STEAM_0:1:11898431', 'STEAM_0:0:54543754', 'STEAM_1:1:.....', 'STEAM_1:1:.....', 'STEAM_1:1:.....'];
             const testRoster = new Roster(rosterArray);
-
-            fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, testRoster.config, async err => {
-                if (err) return console.error(err);
-                console.log('tunkataan servu ylös')
-                child.stdin.write(`./srcds_run -condebug -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${args[1]} +maxplayers 12 +sv_setsteamaccount ${CSGO_srv_token} +get5_scrim\n`);
-                const msg = await message.channel.send('Testinks');
-                connectString()
-                bot.setTimeout(() => {
-                    msg.edit(`\`${connect}\``);
-                }, 2000);
-            });
+            startServer(testRoster, args[1], message, child, bot);
         }
 
-        /*if (args[0] === 'write') {
-            fs.readFile('./games/scrim.json', (err, content) => {
-                if (err) return console.error(err);
-                const roster = JSON.parse(content);
-                if (Object.values(roster.team1).length != 5 && Object.values(roster.team2).length != 5) return message.channel.send('Not enough players for a scrim.');
-                if (!Object.keys(roster.team1).includes(message.author.id) && !Object.keys(roster.team2).includes(message.author.id)) return message.channel.send('You are not on the scrim roster. Sod off.');
-
-                const rosterArray = Object.values(roster.team1);
-                const scrimRoster = new Roster(rosterArray);
-
-                fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, scrimRoster.config, async err => {
-                    if (err) return console.error(err);
-                    message.channel.send('Rosteri kirjoitettu `./srcds_run -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map [map name] +maxplayers 12`');
-                });
-            });
-        }*/
     }
+}
+/**
+* Starts the server. 
+* @param scrimRoster {object} object of scrimRoster class
+* @param map {string}, map to be played
+* @param message {object} discordjs message class object 
+* @param bot {object} discordjs bot class object
+* @param child {objec} sh instance
+*/
+function startServer(scrimRoster, map, message, bot, child) {
+    fs.writeFile(`${CSGO_PATH}/csgo/addons/sourcemod/configs/get5/scrim_template.cfg`, scrimRoster.config, async err => {
+        if (err) { return console.error(err) };
+        child.stdin.write(`./srcds_run -game csgo -tickrate 128 -net_port_try 1 -console -usercon +game_type 0 +game_mode 1 +map ${map} +maxplayers 12 +sv_setsteamaccount ${CSGO_srv_token}\n`);
+        const msg = await message.channel.send('Sierra hyrskyttää...');
+        connectString()
+        bot.setTimeout(() => {
+            msg.edit(`\`${connect}\``);
+        }, 10000);
+    });
+}
+
+function clearRoster(message) {
+    const roster = {
+        "team1": {},
+        "team2": {}
+    };
+    fs.writeFile('./games/scrim.json', JSON.stringify(roster, null, '\t'), err => {
+        if (err) return console.error(err);
+        message.channel.send('Scrimin rosteri tyhjennetty.');
+    });
+}
+
+function clearTeams() {
+    team1 = [];
+    team2 = [];
+}
+/**
+* team should be either "team1" or "team2"
+* @param team {string} team, "team1" or "team2"
+* @param mentionedUserId {discordUserId}, user id of the discord user
+* @param mentionedUserName {string}, username that matches that string
+* @param message {object} discord message class object 
+*/
+function addToTeam(team, mentionedUserId, mentionedUserName, message) {
+    fs.readFile('./games/scrim.json', async (err, content) => {
+        let roster = JSON.parse(content);
+        if (err) return console.error(err);
+        delete roster[team][mentionedUserId];
+        fs.readFile('./games/steamid.json', (err, content) => {
+            if (err) return console.error(err);
+            const steamid = JSON.parse(content);
+            const discordid = mentionedUserId;
+            roster[team][discordid] = steamid[discordid];
+            fs.writeFile('./games/scrim.json', JSON.stringify(roster, null, '\t'), err => {
+                if (err) return console.error(err);
+                /*Shit code to show the teams on "frontend" */
+                team1 = team1.filter(name => { return name != mentionedUserName })
+                team2 = team2.filter(name => { return name != mentionedUserName })
+                if (team === 'team1') { team1.push(mentionedUserName) }
+                if (team === 'team2') { team2.push(mentionedUserName) }
+                const embed = new Discord.MessageEmbed().setTitle('5v5 Scrim Roster').setColor('#ff5555')
+                    .addField(`Tiimi 1 - ${team1.length}`, team1.join('\n') || 'empty', true)
+                    .addField(`Tiimi 2 - ${team2.length}`, team2.join('\n') || 'empty', true);
+                message.channel.send(embed);
+            });
+        });
+    });
 }
